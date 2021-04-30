1054de95ea9d538157cabd9620ef9d5ef8bd8ea5
gaoyuhao <gaoyh1991@gmail.com>
2021-04-30 14:48:43 +0800

    webpack打包Libraries 还需要继续学习

--- example/bandle/webpack-example3/webpack.config.js
+++ example/bandle/webpack-example3/webpack.config.js
@@ -1,1 +1,24 @@
 // 创建 Libraries
+
+const path = require('path')
+
+
+module.exports = {
+    entry:'./src/index.js',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'webpack-numbers.js',
+        library:{
+            name:'webpacknumber',
+            type:'umd' // commonjs AMD 模块支持
+        }
+    },
+    externals:{ // ???? 进一步学习
+        lodash:{
+            commonjs:'lodash',
+            commonjs2:'lodash',
+            amd:'lodash',
+            root:'_'
+        }
+    }
+}
